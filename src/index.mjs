2aa69d970e175bf3e0e06c0ee0f2b1098976a000
eXory2024 <exory@anio.software>
2024-04-03 07:59:27 +0200

    Replace @anio-node-foundation/fs-api with @anio-fs/api.

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,12 @@
-import createFSObject from "@anio-node-foundation/fs-api"
+import fs from "@anio-fs/api"
 
 import sync_impl from "./auto/sync.mjs"
 import async_impl from "./auto/async.mjs"
 
-const async_fs = createFSObject({sync: false})
-const sync_fs = createFSObject({sync: true})
-
 export function scandir(path, options = {}) {
-	return async_impl(async_fs, path, options)
+	return async_impl(fs.async, path, options)
 }
 
 export function scandirSync(path, options = {}) {
-	return sync_impl(sync_fs, path, options)
+	return sync_impl(fs.sync, path, options)
 }
