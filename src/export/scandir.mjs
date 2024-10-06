4981f5839ef5549923a0c2d1ede09a1b69df0b52
eXory2024 <exory@anio.software>
2024-10-07 00:30:51 +0200

    Ignore typescript diagnostics.

--- src/export/scandir.mjs
+++ src/export/scandir.mjs
@@ -3,5 +3,6 @@ import factory from "../auto/export/scandirFactory.mjs"
 const impl = factory()
 
 export default async function scandir(...args) {
+	// @ts-ignore:next-line
 	return await impl(...args)
 }
--- src/export/scandirSync.mjs
+++ src/export/scandirSync.mjs
@@ -3,5 +3,6 @@ import factory from "../auto/export/scandirSyncFactory.mjs"
 const impl = factory()
 
 export default function scandirSync(...args) {
+	// @ts-ignore:next-line
 	return impl(...args)
 }
