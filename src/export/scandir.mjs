0d703f1162d70429a6012637d85ea1c8e6f53d13
eXory2024 <exory@anio.software>
2024-10-08 14:27:46 +0200

    Remove src/export.

--- src/export/scandir.mjs
+++
@@ -1,8 +0,0 @@
-import factory from "../auto/export/scandirFactory.mjs"
-
-const impl = factory()
-
-export default async function scandir(...args) {
-	// @ts-ignore:next-line
-	return await impl(...args)
-}
--- src/export/scandirSync.mjs
+++
@@ -1,8 +0,0 @@
-import factory from "../auto/export/scandirSyncFactory.mjs"
-
-const impl = factory()
-
-export default function scandirSync(...args) {
-	// @ts-ignore:next-line
-	return impl(...args)
-}
