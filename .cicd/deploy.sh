b7bd1f54467611b288edb1778c93ff8f0f85c84e
eXory2024 <exory@anio.software>
2024-10-08 23:32:50 +0200

    Update .cicd/deploy.sh.

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -euf
 
 # Needed for experimental releases
-npm version "$RELEASE_VERSION" --git-tag-version false
+# --allow-same-version to make normal releases work (where version is already set in package.json)
+npm version "$RELEASE_VERSION" --git-tag-version false --allow-same-version
 
 npm publish --provenance --access public
