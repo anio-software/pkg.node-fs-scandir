1610d2e690b7830da68d37a1d6d591dd10ecca5c
eXory2024 <exory@anio.software>
2024-10-08 14:34:13 +0200

    Use generateSyncAsyncVariant.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,11 +1,11 @@
-import {generateFromTemplate} from "fourtune/autogenerate"
+import {generateSyncAsyncVariant} from "fourtune/autogenerate"
 
 export default {
 	realm: "js",
 	type: "package",
 
 	autogenerate: {
-		"export/scandirFactory.mts": generateFromTemplate("template/scandirFactory.mts", {}),
-		"export/scandirSyncFactory.mts": generateFromTemplate("template/scandirFactory.mts", {})
+		"export/scandirFactory.mts": generateSyncAsyncVariant("template/scandirFactory.mts", "async"),
+		"export/scandirSyncFactory.mts": generateSyncAsyncVariant("template/scandirFactory.mts", "sync")
 	}
 }
