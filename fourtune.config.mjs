2715e6d979a9e7c89f9e6c3c5b45bf4949c02a15
eXory2024 <exory@anio.software>
2024-11-09 18:27:50 +0100

    generateSyncAsyncVariant was renamed to generateAsyncSyncVariant.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,12 +1,12 @@
-import {generateSyncAsyncVariant} from "fourtune/autogenerate"
+import {generateAsyncSyncVariant} from "fourtune/autogenerate"
 
 export default {
 	realm: "js",
 	type: "package:async/sync",
 
 	autogenerate: {
-		"export/ScandirOptionsType.d.mts": generateSyncAsyncVariant("template/ScandirOptionsType.d.mts", "async"),
-		"export/ScandirSyncOptionsType.d.mts": generateSyncAsyncVariant("template/ScandirOptionsType.d.mts", "sync"),
+		"export/ScandirOptionsType.d.mts": generateAsyncSyncVariant("template/ScandirOptionsType.d.mts", "async"),
+		"export/ScandirSyncOptionsType.d.mts": generateAsyncSyncVariant("template/ScandirOptionsType.d.mts", "sync"),
 	},
 
 	target: {
