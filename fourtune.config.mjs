cfce0641f29f335be9250b457c6a17aac8c227cc
eXory2024 <exory@anio.software>
2024-11-14 00:16:08 +0100

    Its now sufficient to just place the aysnc/sync template file inside src/template/async.sync/scandir.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,13 +1,6 @@
-import {generateAsyncSyncVariant} from "fourtune/autogenerate"
-
 export default {
 	realm: "js",
 	type: "package:async/sync",
-
-	autogenerate: {
-		"src/export/ScandirOptions.d.mts": generateAsyncSyncVariant("src/template/ScandirOptions.d.mts", "async"),
-		"src/export/ScandirSyncOptions.d.mts": generateAsyncSyncVariant("src/template/ScandirOptions.d.mts", "sync"),
-	},
 
 	target: {
 		function_name: "scandir",
