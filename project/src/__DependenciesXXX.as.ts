45ef2976aaa5145dc350411267bb1b285b0fa0a6
eXory2024 <exory@anio.software>
2025-06-27 00:22:01 +0200

    Add Dependencies type.

---
+++ project/src/__DependenciesXXX.as.ts
@@ -0,0 +1,7 @@
+import type {getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
+//>import type {getTypeOfPathSync} from "@anio-software/pkg.node-fs-path-type"
+
+export type __EnkoreFunctionDependencies = {
+	getTypeOfPath: typeof getTypeOfPath
+//>	getTypeOfPath: typeof getTypeOfPathSync
+}
