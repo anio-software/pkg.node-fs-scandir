697641bdac30bc259d120bf61df0a123724e4fba
eXory2024 <exory@anio.software>
2025-06-27 00:12:48 +0200

    Add __ScandirXXXBaseOptions.

---
+++ project/src/__ScandirXXXBaseOptions.as.ts
@@ -0,0 +1,28 @@
+import type {ScandirEntry} from "#~export/ScandirEntry.ts"
+
+type Filter = {
+	(entry: ScandirEntry): Promise<boolean> | boolean;
+//>	(entry: ScandirEntry): boolean & {then?: never};
+}
+
+export type __XX__ = {
+	/**
+	 * @description
+	 * This option can be set to filter entries.
+	 */
+	filter?: Filter
+
+	/**
+	 * @description
+	 * This flag controls whether to first report directories (default)
+	 * or files.
+	 */
+	reverse?: boolean
+
+	/**
+	 * @description
+	 * Specifies whether the entry path can be missing.
+	 * The default is `false`.
+	 */
+	allowMissingDir?: boolean
+}
