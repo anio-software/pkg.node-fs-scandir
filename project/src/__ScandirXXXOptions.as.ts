608a18fdc7c837d967ab71f1ab9f0e412eea800d
eXory2024 <exory@anio.software>
2025-06-27 00:18:56 +0200

    Add __ScandirXXXOptions.

---
+++ project/src/__ScandirXXXOptions.as.ts
@@ -0,0 +1,12 @@
+import type {ScandirCommonOptions} from "#~src/ScandirCommonOptions.ts"
+//>import type {ScandirSyncCommonOptions} from "#~src/ScandirSyncCommonOptions.ts"
+
+export type __XX__ = ScandirCommonOptions & {
+//>export type __XX__ = ScandirSyncCommonOptions & {
+	/**
+	 * @description
+	 * Specifies whether returned array of entries should be sorted or not.
+	 * The default is `false`.
+	 */
+	sorted?: boolean
+}
