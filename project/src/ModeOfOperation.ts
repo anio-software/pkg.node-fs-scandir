0678ca262da18f35ac6a2917b8e956b63bbcda31
eXory2024 <exory@anio.software>
2025-06-27 00:28:32 +0200

    Add ModeOfOperation.

---
+++ project/src/ModeOfOperation.ts
@@ -0,0 +1,4 @@
+export type ModeOfOperation = "scandir"         |
+                              "scandirExt"      |
+                              "scandirCallback" |
+                              "scandirMapped"
