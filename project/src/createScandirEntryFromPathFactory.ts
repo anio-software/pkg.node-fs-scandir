0c0f29fcd8801af4e590268722a285b60c8ff328
eXory2024 <exory@anio.software>
2025-06-27 15:06:17 +0200

    Take existing implementation from @aniojs/node-fs-scandir.

--- project/src/createScandirEntryFromPathFactory.ts
+++ project/src/createScandirEntryFromPathFactory.ts
@@ -1,9 +1,30 @@
 import type {Ret as ScandirExtRet} from "#~src/scandirSyncExt.ts"
+import path from "node:path"
+import {parents} from "#~src/parents.ts"
+import {realpathSync} from "@anio-software/pkg-private.node-consistent-fs/sync"
 
 export function createScandirEntryFromPathFactory(
 	inputDir: string
 ): ScandirExtRet["createScandirEntryFromPath"] {
 	return function(filePath) {
-		return {} as any
+		const normalizedRootDir = path.normalize(inputDir)
+		const normalizedFilePath = path.normalize(filePath)
+
+		if (!normalizedFilePath.startsWith(normalizedRootDir)) {
+			throw new Error(
+				`Relative file path '${normalizedFilePath}' must start with '${normalizedRootDir}'.`
+			)
+		}
+
+		const relativePath = path.relative(normalizedRootDir, normalizedFilePath)
+
+		return {
+			type: "file:regular",
+			parents: parents(relativePath),
+			name: path.basename(filePath),
+			path: normalizedFilePath,
+			relativePath,
+			absolutePath: realpathSync(filePath)
+		}
 	}
 }
