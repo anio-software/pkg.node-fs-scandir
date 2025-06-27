6c5a11e8a2f6e64a249435eb97acb2699abf9ccd
eXory2024 <exory@anio.software>
2025-06-27 12:04:18 +0200

    Add __scandirXXX.

---
+++ project/src/__scandirXXX.as.ts
@@ -0,0 +1,30 @@
+import type {EnkoreJSRuntimeContextOptions} from "@anio-software/enkore.js-runtime"
+
+import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/Dependencies.ts"
+//>import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/DependenciesSync.ts"
+
+import type {ScandirOptions as Options} from "#~export/ScandirOptions.ts"
+//>import type {ScandirSyncOptions as Options} from "#~export/ScandirSyncOptions.ts"
+
+import {internalImplementation as impl} from "#~src/internalImplementation.ts"
+//>import {internalImplementationSync as impl} from "#~src/internalImplementationSync.ts"
+
+import type {ScandirEntry} from "#~export/ScandirEntry.ts"
+
+export async function scandir(
+//>export function scandir(
+	contextOptions: EnkoreJSRuntimeContextOptions,
+	dependencies: Dependencies,
+	options?: Options
+): Promise<ScandirEntry[]> {
+//>): ScandirEntry[] {
+	const result = await impl(
+//>	const result = impl(
+		contextOptions,
+		dependencies,
+		"scandir",
+		options
+	)
+
+	return []
+}
