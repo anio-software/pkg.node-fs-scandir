1bec9ad30cf85039227389a0fe9744007d893ea4
eXory2024 <exory@anio.software>
2025-06-27 14:49:22 +0200

    Copy existing implementation from @aniojs/node-fs-scandir with some adjustments:
    - fix camel_case
    - use new variable names
    
    Still needs some parts to be ported (scandirMapped and scandirCallback cases).

--- project/src/__internalImplementationXXX.as.ts
+++ project/src/__internalImplementationXXX.as.ts
@@ -10,13 +10,16 @@ import type {AllOptions} from "#~src/getOptions.ts"
 //>import type {AllOptions} from "#~src/getOptionsSync.ts"
 import {validateInputOptions} from "#~src/validateInputOptions.ts"
 //>import {validateSyncInputOptions as validateInputOptions} from "#~src/validateSyncInputOptions.ts"
-import {realpath} from "@anio-software/pkg-private.node-consistent-fs/async"
-//>import {realpath} from "@anio-software/pkg-private.node-consistent-fs/sync"
+import {readdir, realpath} from "@anio-software/pkg-private.node-consistent-fs/async"
+//>import {readdir, realpath} from "@anio-software/pkg-private.node-consistent-fs/sync"
 
 import type {ModeOfOperation} from "#~src/ModeOfOperation.ts"
+import type {ScandirEntry} from "#~export/ScandirEntry.ts"
 import type {ReturnMap} from "#~src/ReturnMap.ts"
 import {getEmptyReturnValue} from "#~src/getEmptyReturnValue.ts"
+import type {ValidPathType} from "@anio-software/pkg.node-fs-path-type"
 import path from "node:path"
+import {parents} from "#~src/parents.ts"
 
 type Options = ReturnType<typeof validateInputOptions>
 
@@ -31,6 +34,80 @@ async function scandirImplementation(
 	result: (any[])|undefined
 ) {
 	const {options, type: optionsType} = userOptions
+
+	const {getTypeOfPath} = dependencies
+	const entries = await readdir(
+//>	const entries = readdir(
+		path.join(resolvedInputDir, relativeEntryDir)
+	)
+
+	for (const entry of entries) {
+		const absolutePath = path.join(resolvedInputDir, relativeEntryDir, entry)
+		const relativePath = path.join(relativeEntryDir, entry)
+
+		const pathType = await getTypeOfPath(absolutePath)
+//>		const pathType = getTypeOfPath(absolutePath)
+
+		const handleCurrentEntry = async () => {
+			const data: ScandirEntry = {
+				type: pathType as ValidPathType,
+				parents: parents(relativePath),
+				name: entry,
+				path: path.join(
+					normalizedInputDir, relativePath
+				),
+				relativePath,
+				absolutePath
+			}
+
+			if (typeof options.filter === "function") {
+				const keep = await options.filter(data)
+//>				const keep = options.filter(data)
+
+				if (keep !== true) return
+			}
+
+			if (typeof options.callback === "function") {
+				await options.callback(data)
+//>				options.callback(data)
+
+				return
+			}
+
+			if (typeof options.map === "function") {
+				(result as any[]).push(await options.map(data))
+//>				(result as any[]).push(options.map(data))
+			} else {
+				(result as any[]).push(data)
+			}
+		}
+
+		const recurse = async () => {
+//>		const recurse = () => {
+			if (pathType !== "dir:regular") return
+
+			await scandirImplementation(
+//>			scandirImplementation(
+				context,
+				normalizedInputDir,
+				resolvedInputDir,
+				relativePath,
+				userOptions,
+				dependencies,
+				result
+			)
+		}
+
+		if (options.reverse === true) await recurse()
+//>		if (options.reverse === true) recurse()
+
+		await handleCurrentEntry()
+//>		handleCurrentEntry()
+
+		// written this way so "if statement" has same length as options.reverse === true
+		if (options.reverse !== true) await recurse()
+//>		if (options.reverse !== true) recurse()
+	}
 }
 
 export async function __XX__<T extends ModeOfOperation>(
