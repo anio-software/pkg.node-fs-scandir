55467eb37a7fc12fc85a796a995681a42cbb3917
eXory2024 <exory@anio.software>
2025-06-27 12:56:56 +0200

    Refactor into separate file.

---
+++ project/src/getEmptyReturnValue.ts
@@ -0,0 +1,22 @@
+import type {ModeOfOperation} from "#~src/ModeOfOperation.ts"
+import type {Ret as ScandirExtRet} from "#~src/scandirSyncExt.ts"
+import type {ReturnMap} from "#~src/ReturnMap.ts"
+
+export function getEmptyReturnValue<T extends ModeOfOperation>(mode: T): ReturnMap[T] {
+	switch (mode) {
+		case "scandir":
+		case "scandirMapped": {
+			return [] as any
+		}
+
+		case "scandirExt": {
+			const ret: ScandirExtRet = {
+				entries: []
+			}
+
+			return ret as any
+		}
+	}
+
+	return undefined as any
+}
