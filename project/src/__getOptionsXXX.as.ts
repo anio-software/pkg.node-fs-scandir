4c40a6b32b5cf0ff48ac6fe01ac556a62c7472f0
eXory2024 <exory@anio.software>
2025-06-27 11:52:06 +0200

    Provide a function that detects the option type and returns a union so we can do type narrowing later.

---
+++ project/src/__getOptionsXXX.as.ts
@@ -0,0 +1,40 @@
+import type {ScandirOptions as Options} from "#~export/ScandirOptions.ts"
+//>import type {ScandirSyncOptions as Options} from "#~export/ScandirSyncOptions.ts"
+
+import type {ScandirCallbackOptions as CallbackOptions} from "#~export/ScandirCallbackOptions.ts"
+//>import type {ScandirSyncCallbackOptions as CallbackOptions} from "#~export/ScandirSyncCallbackOptions.ts"
+
+import type {ScandirMappedOptions as MappedOptions} from "#~export/ScandirMappedOptions.ts"
+//>import type {ScandirSyncMappedOptions as MappedOptions} from "#~export/ScandirSyncMappedOptions.ts"
+
+type AllOptions = Options | CallbackOptions | MappedOptions<unknown>
+
+type Ret = {
+	type: "scandir"
+	options: Options
+} | {
+	type: "scandirCallback"
+	options: CallbackOptions
+} | {
+	type: "scandirMapped"
+	options: MappedOptions<unknown>
+}
+
+export function __XX__(userOptions: AllOptions): Ret {
+	if ("callback" in userOptions) {
+		return {
+			type: "scandirCallback",
+			options: userOptions as CallbackOptions
+		}
+	} else if ("map" in userOptions) {
+		return {
+			type: "scandirMapped",
+			options: userOptions as MappedOptions<unknown>
+		}
+	}
+
+	return {
+		type: "scandir",
+		options: userOptions as Options
+	}
+}
