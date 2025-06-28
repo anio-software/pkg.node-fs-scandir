e746aa1d896ef5af8db32a718faeaad716daaef9
eXory2024 <exory@anio.software>
2025-06-28 16:38:35 +0200

    Add option ignoreErrors.

--- project/src/__ScandirXXXOptions.as.ts
+++ project/src/__ScandirXXXOptions.as.ts
@@ -11,4 +11,13 @@ export type __XX__ = ScandirCommonOptions & {
 	sort?: "alphabetical:ascending"  |
 	       "alphabetical:descending" |
 	       false
+
+	/**
+	 * @description
+	 * Normally scandir() will re-throw errors that occur during directory traversal.
+	 * We can stop this from happening by setting this flag to true.
+	 * Note that this will ignore all errors.
+	 * If more control over errors is desired, use scandirExt() instead.
+	 */
+	ignoreErrors?: boolean
 }
